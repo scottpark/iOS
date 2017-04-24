f4f72f3c9ac322b95c0f3618c925496bb649c532
scott <wamoon@gmail.com>
2017-04-25 06:09:58 +1000

    add fetch request feature

---
+++ diary/diary/Article+CoreDataClass.swift
@@ -0,0 +1,9 @@
+//
+//  Article+CoreDataClass.swift
+//  diary
+//
+//  Created by Scott on 21/4/17.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import Foundation
---
+++ diary/diary/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  diary
+//
+//  Created by Scott on 19/04/2017.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
