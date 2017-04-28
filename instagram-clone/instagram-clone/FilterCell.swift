0fab61ed07f95b9d37a240dc110be64b446e85fd
scott <wamoon@gmail.com>
2017-04-29 05:12:30 +1000

    create filtered images

---
+++ instagram-clone/instagram-clone/FilterCell.swift
@@ -0,0 +1,13 @@
+//
+//  FilterCell.swift
+//  instagram-clone
+//
+//  Created by Scott on 28/4/17.
+//  Copyright © 2017 Scott. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+}
